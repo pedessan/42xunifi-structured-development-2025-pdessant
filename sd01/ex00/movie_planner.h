a7a6aa7f5077712727ec9d2c7277501f687f306c
Petra Dessanti <pdessant@c2r6p2.42firenze.it>
2025-06-11 16:07:25 +0200

    sd01 ex00 added

---
+++ sd01/ex00/movie_planner.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movie_planner.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdessant <pdessant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 15:43:50 by pdessant          #+#    #+#             */
+/*   Updated: 2025/06/11 16:00:27 by pdessant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MOVIE_PLANNER_H
+# define MOVIE_PLANNER_H
+
+# include <stdlib.h>
+// Returns user preferences. Returns NULL on failure.
+struct Preferences *get_user_preferences(void);
+
+// Returns a list of movies matching the given preferences.
+//      Returns NULL on failure.
+struct MovieList *find_movies(struct Preferences *prefs);
+
+// Returns a movie night plan from the given list. Returns NULL on failure.
+struct Plan *build_plan(struct MovieList *list);
+
+struct Plan *create_movie_night_plan(void);
+
+#endif
