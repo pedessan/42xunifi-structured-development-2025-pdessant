e8848060a6a2561cb174661e2518b9f53d25be84
Petra Dessanti <pdessant@c2r6p2.42firenze.it>
2025-06-11 16:42:00 +0200

    sd01 ex01 added but to be reviewed

---
+++ sd01/ex01/food_order.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   food_order.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdessant <pdessant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 16:10:20 by pdessant          #+#    #+#             */
+/*   Updated: 2025/06/11 16:37:02 by pdessant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "food_order.h"
+
+/* 
+You’re building the backend for a food delivery app. A critical task is to process an in-
+coming order. The logic changes depending on whether the restaurant is currently open
+or closed. If it’s open, you create a standard order confirmation. If it’s closed, you create
+a pre-order confirmation for the next day.
+Your goal is to implement the control logic that, given an order request, checks the restau-
+rant’s status, prepares the appropriate confirmation, and sends a notification.
+*/
+
+int process_food_order(struct OrderRequest *request)
+{
+    struct OrderConfirmation *confirmation;
+    int status = check_restaurant_status(request);
+
+    if (status == 1)
+        confirmation = create_standard_confirmation();
+
+    else if (status == 0)
+        confirmation = create_preorder_confirmation();
+    if (!confirmation)
+    {
+        free(request);
+        return (-1);
+    }
+
+    send_confirmation_notification(confirmation);
+    return (0);
+}
