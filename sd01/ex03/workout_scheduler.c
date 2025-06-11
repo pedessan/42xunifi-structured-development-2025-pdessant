af8c414e02d57d24a521401713dbc86ff797058a
Petra Dessanti <pdessant@c2r6p2.42firenze.it>
2025-06-11 17:18:41 +0200

    base ready for sd01 ex03 to be worked on

---
+++ sd01/ex03/workout_scheduler.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   workout_scheduler.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdessant <pdessant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 17:14:35 by pdessant          #+#    #+#             */
+/*   Updated: 2025/06/11 17:17:08 by pdessant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "workout_scheduler.h"
+
+/* 
+You’re building a workout scheduler for a fitness tracking app. The system fetches a
+user’s data, builds a preliminary workout plan, refines it with the user’s preferences, and
+then finalizes a complete schedule for a certain number of days. Each day includes per-
+sonalized exercises and motivational tips.
+*/
