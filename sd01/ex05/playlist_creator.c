a5f34c8420c40ca7bc032ce3fc68b7b9d52a97d7
Petra Dessanti <pdessant@c2r5p7.42firenze.it>
2025-06-12 16:13:00 +0200

    sd01 ex05 added but to be reviewed

--- sd01/ex05/playlist_creator.c
+++ sd01/ex05/playlist_creator.c
@@ -6,7 +6,7 @@
 /*   By: pdessant <pdessant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 11:47:54 by pdessant          #+#    #+#             */
-/*   Updated: 2025/06/12 12:17:01 by pdessant         ###   ########.fr       */
+/*   Updated: 2025/06/12 15:31:09 by pdessant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,41 @@ You are building a personalized playlist generator for a music app. The program 
 
 struct Playlist *create_playlist(void)
 {
+    int i = 0;
     struct SongData *song;
     struct MoodSettings     *mood = analyze_user_mood();
     struct FilterSettings   *filter = default_filters();
 
+    if (!filter)
+    {
+        free_mood_settings(mood);
+        return (NULL);
+    }
+
     int variations = get_mood_variations(mood);
-    struct FilterSettings   *refined = refine_filters(filter);
-    int popular = filters_require_popular_song(refined);
+    while (i < variations)
+    {
+        struct FilterSettings   *refined = refine_filters(filter);
+        if (!refined)
+        {
+            free_mood_settings(mood);
+            free_filter_settings(filter);
+            return (NULL);
+        }
+        free_filter_settings(filter);  // libera il precedente
+        filter = refined;              // aggiorna il puntatore a quello nuovo
+        i++;
+    }
+    int popular = filters_require_popular_song(filter);
+    free_filter_settings(filter);
     if (popular != 0)
         song = fetch_popular_song();
     else
         song = fetch_niche_song();
+    struct Playlist *playlist = combine_with_mood_playlist(song, mood);
+    free_mood_settings(mood);
+    free_song_data(song);
+    if (!playlist)
+        return (NULL);
+    return (playlist);
 }
