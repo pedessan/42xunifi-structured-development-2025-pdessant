f3dbd6816a2386cf90a6cdbb81aedede36529477
Petra Dessanti <pdessant@c2r5p7.42firenze.it>
2025-06-12 17:19:22 +0200

    sd01 ex06 almost done but still in progress

--- sd01/ex06/task_scheduler.c
+++ sd01/ex06/task_scheduler.c
@@ -6,7 +6,7 @@
 /*   By: pdessant <pdessant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 16:16:47 by pdessant          #+#    #+#             */
-/*   Updated: 2025/06/12 16:35:55 by pdessant         ###   ########.fr       */
+/*   Updated: 2025/06/12 17:18:26 by pdessant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,17 @@ ing the following prototypes:
 
 struct ScheduleResult *schedule_tasks(struct TaskList *tasks)
 {
+    int i = 0;
     struct TaskProfile  *profiled = profile_tasks(tasks);
     struct PriorityMap  *new_priority = compute_priorities_mock(profiled);
     int cpu = get_available_core_count();
-    
+    i = 0;
+    while (i < cpu)
+    {
+        struct ScheduleResult   *res = create_schedule_result(cpu);
+        int best_task = select_best_task(profiled);
+        update_schedule_entry(res, i, best_task);
+        i++;
+    }
+    return (res);
 }
